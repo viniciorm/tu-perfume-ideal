d3c344187cb49f929a93452ec0c557d79a66ac49
viniciorm <marcos.reyes.m@gmail.com>
2026-06-04 11:54:03 -0400

    Make FathersDayLanding the home page and move full catalog to /catalogo

--- src/App.tsx
+++ src/App.tsx
@@ -73,12 +73,13 @@ export default function App() {
       <Header />
       
       <Routes>
+        <Route path="/" element={<FathersDayLanding onAddProduct={handleSelectProduct} />} />
         <Route path="/hombres" element={<FathersDayLanding onAddProduct={handleSelectProduct} />} />
         <Route path="/dia-del-padre" element={<FathersDayLanding onAddProduct={handleSelectProduct} />} />
         <Route path="/mujer" element={<WomenPerfumesLanding onAddProduct={handleSelectProduct} />} />
         <Route path="/packs" element={<PacksLanding onAddProduct={handleSelectProduct} />} />
         
-        <Route path="/" element={
+        <Route path="/catalogo" element={
           <>
             <main className="container mx-auto px-4 py-8">
               <div className="mb-12">
